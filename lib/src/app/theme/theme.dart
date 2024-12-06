ea1ad981226d07efadc9e1f094c643d3557deb1e
almajd3713 <almajd3713@gmail.com>
2024-12-06 14:43:41 +0100

    add font changes

--- lib/src/app/theme/theme.dart
+++ lib/src/app/theme/theme.dart
@@ -2,13 +2,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:smart_lifters/src/app/theme/colors.dart';
+import 'package:smart_lifters/src/app/theme/font.dart';
 
 class AppTheme {
   static final ThemeData lightTheme = ThemeData(
-    colorScheme: AppColors.lightScheme
+    colorScheme: AppColors.lightScheme,
+    textTheme: AppFonts.textScheme
   );
 
   static final ThemeData darkTheme = ThemeData(
-    colorScheme: AppColors.darkScheme
+    colorScheme: AppColors.darkScheme,
+    textTheme: AppFonts.textScheme
   );
 }
